14e3f321a9b10f54263898243cdcbc8d5fc0e8ca
Tharun Raj <tharun.2.linux@gmail.com>
2024-06-02 23:10:58 +0530

    add new sidebar

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const navigationToggleButton = document.getElementById("navToggleBtn");
 const navigationLinks = document.querySelector(".navbar__links");
-let listIcon = document.getElementById("togglerImage");
 let hidden = true;
+const sidebar = document.querySelector(".sidebar");
+const dismissButton = document.querySelector("#dismissButton");
 
 navigationToggleButton.addEventListener("click", () => {
-  if (hidden) {
-    navigationLinks.classList.add("navbar__links-show");
-    listIcon.src = "./assets/images/icon-menu-close.svg";
-    hidden = false;
-    // navigationToggleButton.classList.add("navbar__toggle-button-active");
-  } else {
-    navigationLinks.classList.remove("navbar__links-show");
-    // navigationToggleButton.classList.remove("navbar__toggle-button-active");
-    listIcon.src = "./assets/images/icon-menu.svg";
-    hidden = true;
-  }
+  sidebar.classList.add("sidebar-show");
+});
+
+dismissButton.addEventListener("click", () => {
+  sidebar.classList.remove("sidebar-show");
 });
